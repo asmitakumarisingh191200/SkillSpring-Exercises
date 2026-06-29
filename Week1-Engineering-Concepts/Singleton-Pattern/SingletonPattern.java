9aea97193271032c0db8da961222d9e40063f28e
Asmita Singh <asmitas304@gmail.com>
2026-06-29 13:49:29 +0530

    Added Singleton Pattern exercise

--- Week1-Engineering-Concepts/Singleton-Pattern/SingletonPattern.java
+++ Week1-Engineering-Concepts/Singleton-Pattern/SingletonPattern.java
@@ -1,1 +1,21 @@
+class Singleton {
 
+    private static Singleton obj = new Singleton();
+
+    private Singleton() {}
+
+    public static Singleton getInstance() {
+        return obj;
+    }
+}
+
+public class SingletonPattern {
+
+    public static void main(String[] args) {
+
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+
+        System.out.println(s1 == s2);
+    }
+}
