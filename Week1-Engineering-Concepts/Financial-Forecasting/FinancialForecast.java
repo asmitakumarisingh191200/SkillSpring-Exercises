b81773a2ea37fa2a721d10e076f47a54e0007165
Asmita Singh <asmitas304@gmail.com>
2026-06-29 22:34:45 +0530

    Completed Financial Forecasting Exercise

--- Week1-Engineering-Concepts/Financial-Forecasting/FinancialForecast.java
+++ Week1-Engineering-Concepts/Financial-Forecasting/FinancialForecast.java
@@ -1,1 +1,24 @@
+public class FinancialForecast {
 
+    public static double forecast(double amount, double rate, int years) {
+
+        
+        if (years == 0) {
+            return amount;
+        }
+
+        
+        return forecast(amount * (1 + rate), rate, years - 1);
+    }
+
+    public static void main(String[] args) {
+
+        double currentAmount = 1000;
+        double growthRate = 0.10; // 10%
+        int years = 3;
+
+        double futureValue = forecast(currentAmount, growthRate, years);
+
+        System.out.println("Future Value after " + years + " years: " + futureValue);
+    }
+}
