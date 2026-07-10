cd020f02cc04fe24dba9d9dc134735c76b35d19d
Asmita Singh <asmitas304@gmail.com>
2026-07-10 18:06:02 +0530

    Convert Week3_SpringREST to normal directory

---
+++ Week3_SpringREST/src/main/java/com/springrest/Week3_SpringREST/Week3SpringRestApplication.java
@@ -0,0 +1,13 @@
+package com.springrest.Week3_SpringREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week3SpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week3SpringRestApplication.class, args);
+	}
+
+}
